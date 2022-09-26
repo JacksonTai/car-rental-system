e85d68e2266c0404e943ae7579ec9f8f1b2e5f52
Jackson Tai <jacksontai21@gmail.com>
2022-09-26 18:28:08 +0800

    Initial folders
    - folder structure from NetBeans with some boilerplates

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+
+import UI.LoginForm;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Jackson
+ */
+public class Main {
+ 
+    public static void main (String[]args) {
+        LoginForm loginForm = new LoginForm();
+        loginForm.setVisible(true);
+    }
+    
+}
