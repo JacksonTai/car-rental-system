045310afe40cde888a92745765b03690572ac70e
Jackson Tai <jacksontai21@gmail.com>
2022-09-27 08:17:51 +0800

    Built base class

---
+++ src/main/java/Car.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Jackson
+ */
+public class Car {
+    
+    private String plateNum;
+    private String model;
+    private String colour;
+    private int pricePerDay;
+    
+    public void setPlateNum(String plateNum) {
+        this.plateNum = plateNum;
+    }
+    
+    public String getPlateNum() {
+        return this.plateNum;
+    }
+    
+    public void setModel(String model) {
+        this.model = model;
+    }
+    
+    public String getModel() {
+        return this.model;
+    }
+       
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+    
+    public String getColour() {
+        return this.colour;
+    }
+    
+    public void setPricePerDay(int pricePerDay) {
+        this.pricePerDay = pricePerDay;
+    }
+    
+    public int getPricePerDay() {
+        return this.pricePerDay;
+    }
+    
+}
